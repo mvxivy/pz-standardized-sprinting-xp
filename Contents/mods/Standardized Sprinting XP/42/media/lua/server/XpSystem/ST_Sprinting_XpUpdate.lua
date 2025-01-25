1c596881df23381d6d2034f2a53e484c3d0898b2
Maksim Ivanov <vonavimax@gmail.com>
2025-01-25 13:32:16 +0700

    refactor: core sprinting

--- Contents/mods/Standardized Sprinting XP/42/media/lua/server/XpSystem/ST_Sprinting_XpUpdate.lua
+++ Contents/mods/Standardized Sprinting XP/42/media/lua/server/XpSystem/ST_Sprinting_XpUpdate.lua
@@ -1,37 +1,27 @@
---****************************************************************************************************************
---**		Created By: 	Conqueror Koala																		**
---**		Mod:			Standardized Sprinting XP															**
---**																											**		
---**		Information:																						**
---**					This code is totally free for you to edit, use, or copy however you want!				**
---**					Feel free to use any of the code in your own projects, don't worry about crediting. 	**
---****************************************************************************************************************
+local modOptions = require("ST_Sprinting_ModOptions")
+modOptions.init()
 
+local sprintMultiplier = {1, 5, 10, 25, 50, 100, 1000};
+local sprintDelay = {250, 500, 1000, 1500, 2000, 3000, 5000, 10000};
 
---************************************************************************************************
---**  Would not recommend changing anything past this point unless you know what you are doing. **
---************************************************************************************************
-local SETTINGS = StandardizedSprintingXP_global.SETTINGS
+-- xpThrottle is used to control the frequency of XP gain
+local xpThrottle = 0;
 
-local sprintMult = {1,5,10,25,50,100,1000};
-local sprintDelay = {250,500,1000,1500,2000,3000,5000,10000};
-
-xpThrottle = 0; -- Just a counting variable.
-
--- used everytime the player moves
-SprintingBonusXP = function()
-xpThrottle = tonumber(xpThrottle);
+local function SprintingBonusXP()
 	local player = getPlayer();
 	local xp = player:getXp();
 	-- if you're running and your endurance has changed
-	if (player:IsRunning() or player:isSprinting()) and player:getStats():getEndurance() > player:getStats():getEndurancewarn() then
+	if 
+		(player:IsRunning() or player:isSprinting())
+		and player:getStats():getEndurance() > player:getStats():getEndurancewarn()
+	then
 		-- you may gain 1 instance of Sprinting XP
-		if tonumber(xpThrottle) > tonumber(sprintDelay[tonumber(SETTINGS.options.dropdown2)]) then
-			xp:AddXP(Perks.Sprinting, tonumber(sprintMult[tonumber(SETTINGS.options.dropdown1)]));
+		if xpThrottle > sprintDelay[modOptions.ComboBoxDelay:getValue()] then
+			xp:AddXP(Perks.Sprinting, sprintMultiplier[modOptions.ComboBoxReward:getValue()]);
 			xpThrottle = 0;
 		end
-		xpThrottle = tonumber(xpThrottle) + 1;
+		xpThrottle = xpThrottle + 1;
 	end
 end
 
-Events.OnPlayerMove.Add(SprintingBonusXP); -- Calls the above function every time player moves.
+Events.OnPlayerMove.Add(SprintingBonusXP);
