ed24a3c28d25395d18ab155cdf46225674071fc8
Maksim Ivanov <vonavimax@gmail.com>
2025-12-18 23:12:11 +0700

    feat: backward compability for 42.12

--- Contents/mods/Standardized Sprinting XP/42/media/lua/client/ST_Sprinting_XpUpdate.lua
+++ Contents/mods/Standardized Sprinting XP/42/media/lua/client/ST_Sprinting_XpUpdate.lua
@@ -6,15 +6,25 @@ local sprintDelay = {250, 500, 1000, 1500, 2000, 3000, 5000, 10000};
 
 -- xpThrottle is used to control the frequency of XP gain
 local xpThrottle = 0;
+local gameVersion = tostring(getCore():getGameVersion())
+
+local function checkBonusCondition()
+	local player = getPlayer();
+
+	if(string.sub(gameVersion, 0, 5) == "42.13") then
+		return (player:IsRunning() or player:isSprinting())
+			and player:getStats():getLastEndurance() > player:getStats():getEnduranceWarning()
+	else 
+		return (player:IsRunning() or player:isSprinting())
+			and player:getStats():getEndurance() > player:getStats():getEndurancewarn()
+	end
+end
 
 local function SprintingBonusXP()
 	local player = getPlayer();
 	local xp = player:getXp();
 	-- if you're running and your endurance has changed
-	if 
-		(player:IsRunning() or player:isSprinting())
-		and player:getStats():getLastEndurance() > player:getStats():getEnduranceWarning()
-	then
+	if checkBonusCondition() then
 		-- you may gain 1 instance of Sprinting XP
 		if xpThrottle > sprintDelay[modOptions.ComboBoxDelay:getValue()] then
 			xp:AddXP(Perks.Sprinting, sprintMultiplier[modOptions.ComboBoxReward:getValue()]);
